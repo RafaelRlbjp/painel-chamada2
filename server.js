c0a1ad3407e22eea37882173e4daa2faa54d7a12
Rafael Lima <rafaellima@MacBook-Pro-de-Rafael.local>
2026-01-31 23:34:29 -0300

    corrigido socket chamada

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const path = require("path");
 
-app.use(express.json());
 app.use(express.static("public"));
 
 let ultimo = null;
@@ -14,24 +13,23 @@ app.get("/", (req,res)=>{
  res.sendFile(path.join(__dirname,"public","painel.html"));
 });
 
-app.post("/chamar",(req,res)=>{
-
- ultimo = {
-  nome:req.body.paciente,
-  profissional:req.body.profissional,
-  consultorio:req.body.consultorio
- };
-
- historico.unshift(ultimo);
- historico = historico.slice(0,5);
-
- io.emit("novoChamado",{ultimo,historico});
-
- res.sendStatus(200);
-});
-
 io.on("connection",(socket)=>{
+
  socket.emit("novoChamado",{ultimo,historico});
+
+ socket.on("chamar",(dados)=>{
+
+   ultimo = dados;
+
+   historico.unshift(dados);
+   historico = historico.slice(0,5);
+
+   io.emit("novoChamado",{ultimo,historico});
+
+ });
+
 });
 
-http.listen(process.env.PORT || 3000);
+http.listen(process.env.PORT || 3000, ()=>{
+ console.log("Servidor rodando");
+});
